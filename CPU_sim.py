d7a126bd0b5debf56696c0339d82b593e5989930
DGRod <DGRodriguezNY@gmail.com>
2024-05-16 15:43:09 -0400

    Added input data and ADD/ADDI/SUB methods

--- CPU_sim.py
+++ CPU_sim.py
@@ -2,10 +2,15 @@
 
 class Register:
 
-    def __init__(self, name):
+    def __init__(self, name, num_of_registers, num_of_history_registers=10):
         self.name = name
-        self.data_registers = [0, 0, 0, 4, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.history_registers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.data_registers = [0 for x in range(0, num_of_registers)]
+        self.history_registers = [0 for x in range(0, num_of_history_registers)]
+
+
+class MemoryBus:
+    
+    
 
 class ALU:
 
@@ -20,18 +25,49 @@ class ALU:
         print(opcode, operands)
 
         if opcode == "ADD":
-            result_register = operands[0]
-            sum = 0
-            for operand in operands:
-                sum += self.register.data_registers[int(operand.strip("R"))]
-            print(sum)
-            return sum
+            self.add(operands)
+        
+        elif opcode == "SUB":
+            self.sub(operands)
+
+        elif opcode == "ADDI":
+            self.addi(operands)
+        
+    def add(self, operands):
+        register = self.register.data_registers
+        rd = int(operands[0].strip("R$"))
+        rs = int(operands[1].strip("R$"))
+        rt = int(operands[2].strip("R$"))
+
+        register[rd] = register[rs] + register[rt]
+        print("Register #"+ str(rd), register[rd])
+        return register[rd]
+    
+    def addi(self, operands):
+        register = self.register.data_registers
+        rd = int(operands[0].strip("R$"))
+        rs = int(operands[1].strip("R$"))
+        immd = int(operands[2].strip("R$"))
+
+        register[rd] = register[rs] + immd
+        print("Register #"+ str(rd), register[rd])
+        return register[rd]
+    
+    def sub(self, operands):
+        register = self.register.data_registers
+        rd = int(operands[0].strip("R$"))
+        rs = int(operands[1].strip("R$"))
+        rt = int(operands[2].strip("R$"))
+
+        register[rd] = register[rs] - register[rt]
+        print("Register #"+ str(rd), register[rd])
+        return register[rd]
 
 
-
-register1 = Register("register1")
-
+register1 = Register("register1", 22)
+register1.data_registers[6] = 4
+register1.data_registers[4] = 5
 
 alu1 = ALU("alu1", register1)
 
-alu1.run("ADD R3,R6")
+alu1.run("ADDI R3,R6,R4")
